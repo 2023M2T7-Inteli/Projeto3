0670003b40f80598dacd530593da0091dbcfcd96
joaosotto <joao.maior@sou.inteli.edu.br>
2023-06-06 22:15:14 -0300

    integração hc finalizada

--- src/frontend/homecoletor/fornece_protocolo.js
+++ src/frontend/homecoletor/fornece_protocolo.js
@@ -1,40 +1,56 @@
 const on_load = () => {
-  le_protocolo();
-  criar_estruturaHtml();
+  adiciona_protocolo();
 };
 
-const le_protocolo = () => {
+const adiciona_protocolo = () => {
   axios
-    .get(" http://127.0.0.1:3000/visualizar_protocolos")
+    .get(" http://localhost:1234/visualizar_protocolos/")
 
     .then((response) => {
       const protocolos = response.data;
 
       console.log(protocolos);
+
+      let array_html_abertas = [];
+      let array_html_enviadas = [];
+
+      protocolos.forEach((protocolo) => {
+        const { nome, descricao, data_limite, estado } = protocolo;
+        let html = "";
+
+        html += `<div class="amostra">
+        <img
+          class="imagens_das_amostras"
+          src="../img/img_fruta_teste.jpg"
+          alt="Fruta 1"
+        />
+        <div class="texto_das_amostras">
+          <h4 class="nome_amostra">${protocolo.NOME}</h4>
+          <p class="validade_amostra">${protocolo.DATA_LIMITE}</p>
+          </div>
+          </div>`;
+
+        if (protocolo.ESTADO === "Em aberto") {
+          array_html_abertas.push(html);
+          // coletas_abertas.innerHTML += html;
+        } else {
+          array_html_enviadas.push(html);
+          // coletas_enviadas.innerHTML += html;
+        }
+
+        console.log(array_html_abertas);
+      });
+      const coletas_abertas = document.getElementById("coletas_abertas");
+      const coletas_enviadas = document.getElementById("coletas_enviadas");
+
+      array_html_abertas.forEach((trecho_html) => {
+        coletas_abertas.innerHTML += trecho_html;
+      });
+      array_html_enviadas.forEach((trecho_html) => {
+        coletas_enviadas.innerHTML += trecho_html;
+      });
     })
     .catch((error) => {
       console.log(error);
     });
-};
-
-const criar_estruturaHtml = () => {
-  let html = "";
-
-  protocolos.forEach((protocolo) => {
-    const { nome, descricao, data_limite, estado } = protocolo;
-
-    html += ` <img
-  class="imagens_das_amostras"
-  src="../img/img_fruta_teste.jpg"
-  alt="Fruta 1"
-/>
-<div class="texto_das_amostras">
-  <h4 class="nome_amostra">${nome}</h4>
-  <p class="validade_amostra">${data_limite}</p>
-</div>`;
-  });
-
-  const container = document.getElementById("#container");
-
-  container.innerHTML += html;
 };

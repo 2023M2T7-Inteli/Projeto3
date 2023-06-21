c90b0d23cdc702aedb601073c6185c488d8fbc1b
joaosotto <joao.maior@sou.inteli.edu.br>
2023-06-21 18:14:37 -0300

    consegui vamoooo

--- src/frontend/responderprotocolo/script.js
+++ src/frontend/responderprotocolo/script.js
@@ -1,83 +1,50 @@
-// const on_load = () => {
-//   adiciona_pergunta();
-// };
-
-// const adiciona_pergunta = () => {
-//   axios
-//     .get("http://localhost:1234/visualizar_perguntas")
-//     .then((response) => {
-//       const perguntas = response.data;
-//       console.log(perguntas);
-
-//       perguntas.forEach((pergunta) => {
-//         //console.log("entra no loop");
-//         let html = "";
-//         html +=
-//           "<div><h4>" +
-//           pergunta.PERGUNTA +
-//           "</h4><input class='resposta'></div>";
-
-//         console.log(html);
-//         document.getElementById("perguntas").innerHTML = html;
-//       });
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// };
-
-// const submeter_respostas = () => {
-//   const respostas = {};
-// };
-
-
 const on_load = () => {
-      adiciona_pergunta();
-    };
+  adiciona_pergunta();
+};
 
-    const adiciona_pergunta = () => {
-      const path = window.location.pathname
-      const id_url = path.split('/').pop()
+const adiciona_pergunta = () => {
+  const path = window.location.pathname;
+  const id_url = path.split("/").pop();
 
-      console.log('ID: ', id_url)
+  console.log("ID: ", id_url);
 
-      axios
-        .get("http://localhost:1234/visualizar_perguntas/" + id_url)
-        .then((response) => {
-          const perguntas = response.data;
-          console.log(perguntas);
+  axios
+    .get("http://localhost:1234/visualizar_perguntas/" + id_url)
+    .then((response) => {
+      const perguntas = response.data;
+      console.log(perguntas);
 
-          perguntas.forEach((pergunta) => {
-            console.log("entra no loop");
-            //dá pra adicionar um if pro tipo da pergunta
-            let html = "";
-            switch (pergunta.TIPO_INPUT) {
-  case "text":
-    html += `
+      perguntas.forEach((pergunta) => {
+        console.log("entra no loop");
+        //dá pra adicionar um if pro tipo da pergunta
+        let html = "";
+        switch (pergunta.TIPO_INPUT) {
+          case "text":
+            html += `
       <div class="input-container">
         <label for="resposta_${pergunta.ID}">${pergunta.PERGUNTA}</label><br><br>
         <input type="text" id="resposta_${pergunta.ID}" class="resposta">
       </div>
     `;
-    break;
-  case "date":
-    html += `
+            break;
+          case "date":
+            html += `
       <div class="input-container">
         <label for="resposta_${pergunta.ID}">${pergunta.PERGUNTA}</label><br><br>
         <input type="date" id="resposta_${pergunta.ID}" class="resposta">
       </div>
     `;
-    break;
-  case "number":
-    html += `
+            break;
+          case "number":
+            html += `
       <div class="input-container">
         <label for="resposta_${pergunta.ID}">${pergunta.PERGUNTA}</label><br><br>
         <input type="number" id="resposta_${pergunta.ID}" class="resposta">
       </div>
     `;
-    break;
-  case "select":
-    html += `
+            break;
+          case "select":
+            html += `
       <div class="input-container">
         <label for="resposta_${pergunta.ID}">${pergunta.PERGUNTA}</label><br><br>
         <select id="resposta_${pergunta.ID}" class="resposta">
@@ -87,33 +54,47 @@ const on_load = () => {
         </select>
       </div>
     `;
-    break;
-  default:
-    // Tipo de resposta desconhecido
-    break;
-}
+            break;
+          default:
+            // Tipo de resposta desconhecido
+            break;
+        }
 
+        console.log(html);
+        document.getElementById("perguntas").innerHTML += html;
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-            console.log(html);
-            document.getElementById("perguntas").innerHTML += html;
-          });
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
+document.addEventListener("DOMContentLoaded", function () {
+  //peguei o valor do input vamoooo
+  var meuBotao = document.getElementById("enviar");
 
-    const submeter_respostas = () => {
-      const respostas = {};
-      const inputs = document.getElementsByClassName("resposta");
-      for (let i = 0; i < inputs.length; i++) {
-        const pergunta = perguntas[i].PERGUNTA;
-        const resposta = inputs[i].value;
-        respostas[pergunta] = resposta;
-      }
-      console.log(respostas);
-      // Aqui você pode enviar as respostas para o servidor usando o Axios ou outra biblioteca de requisição HTTP
-    };
+  meuBotao.addEventListener("click", function () {
+    var meuInput = document.getElementsByClassName("resposta")[0];
+    var valor = meuInput.value;
+    console.log(valor);
+  });
+});
 
+// let dados = document.getElementsByClassName("resposta").value;
+//   console.log(dados);
 
-    //até aqui em cima funciona, agora vamo testar mandar as respostas
+//sla oq e isso
+
+// const submeter_respostas = () => {
+//   const respostas = {};
+//   const inputs = document.getElementsByClassName("resposta");
+//   for (let i = 0; i < inputs.length; i++) {
+//     const pergunta = perguntas[i].PERGUNTA;
+//     const resposta = inputs[i].value;
+//     respostas[pergunta] = resposta;
+//   }
+//   console.log(respostas);
+//   // Aqui você pode enviar as respostas para o servidor usando o Axios ou outra biblioteca de requisição HTTP
+// };
+
+// //até aqui em cima funciona, agora vamo testar mandar as respostas
